bbdc4d76545582297a4965bdb7aaab2f6b8e9ebf
Namhkn12 <vutuannam39@gmail.com>
2025-04-13 20:56:49 +0700

    User Service endpoint change

--- app/src/main/java/api/user/UserApiClient.java
+++ app/src/main/java/api/user/UserApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class UserApiClient {
-    private static final String USER_ENDPOINT_URL = "http://192.168.1.101:8080/api/";
+    private static final String USER_ENDPOINT_URL = "http://14.247.51.251:25501/api/";
     private static Retrofit userRetrofit;
 
     public static Retrofit getRetrofit() {
--- app/src/main/java/com/ndroid/ecommerce/UserDemoActivity.java
+++ app/src/main/java/com/ndroid/ecommerce/UserDemoActivity.java
@@ -32,7 +32,7 @@ public class UserDemoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user_demo);
 
         initGui();
 
