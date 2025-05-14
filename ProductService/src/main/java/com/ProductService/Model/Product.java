8187dca688ffa31b1b58daa2083a9baccd83499e
spleeter <ngdnam03@gmail.com>
2025-05-14 09:50:20 +0700

    init product

---
+++ ProductService/src/main/java/com/ProductService/Model/Product.java
@@ -0,0 +1,27 @@
+package com.ProductService.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+    private String imageUrl;
+    private String category;
+
+}
